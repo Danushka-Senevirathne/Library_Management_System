4b9b0923e82b3b8699c78d9a31e311a34ee10ad5
Danushka-Senevirathne <danush15100@gmail.com>
2025-02-11 16:42:14 +0530

    book view form coding 1st section done

--- View book form.Designer.cs
+++ View book form.Designer.cs
@@ -41,14 +41,15 @@
             button4 = new Button();
             button3 = new Button();
             button2 = new Button();
-            textBox5 = new TextBox();
-            textBox6 = new TextBox();
-            textBox7 = new TextBox();
-            textBox4 = new TextBox();
-            textBox3 = new TextBox();
-            textBox2 = new TextBox();
+            txtBQuantity = new TextBox();
+            txtBPrice = new TextBox();
+            txtBDate = new TextBox();
+            txtBPublisher = new TextBox();
+            txtBAuthor = new TextBox();
+            txtBName = new TextBox();
             dataGridView1 = new DataGridView();
             panel3 = new Panel();
+            label9 = new Label();
             textBox1 = new TextBox();
             btnRefresh = new Button();
             label2 = new Label();
@@ -66,7 +67,7 @@
             panel1.Controls.Add(pictureBox1);
             panel1.Location = new Point(0, 0);
             panel1.Name = "panel1";
-            panel1.Size = new Size(877, 62);
+            panel1.Size = new Size(1145, 62);
             panel1.TabIndex = 0;
             // 
             // label1
@@ -83,7 +84,7 @@
             // pictureBox1
             // 
             pictureBox1.Image = Properties.Resources.book_gif_2;
-            pictureBox1.Location = new Point(790, 0);
+            pictureBox1.Location = new Point(1049, 0);
             pictureBox1.Name = "pictureBox1";
             pictureBox1.Size = new Size(78, 59);
             pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
@@ -102,15 +103,15 @@
             panel2.Controls.Add(button4);
             panel2.Controls.Add(button3);
             panel2.Controls.Add(button2);
-            panel2.Controls.Add(textBox5);
-            panel2.Controls.Add(textBox6);
-            panel2.Controls.Add(textBox7);
-            panel2.Controls.Add(textBox4);
-            panel2.Controls.Add(textBox3);
-            panel2.Controls.Add(textBox2);
-            panel2.Location = new Point(502, 59);
+            panel2.Controls.Add(txtBQuantity);
+            panel2.Controls.Add(txtBPrice);
+            panel2.Controls.Add(txtBDate);
+            panel2.Controls.Add(txtBPublisher);
+            panel2.Controls.Add(txtBAuthor);
+            panel2.Controls.Add(txtBName);
+            panel2.Location = new Point(761, 61);
             panel2.Name = "panel2";
-            panel2.Size = new Size(375, 473);
+            panel2.Size = new Size(383, 473);
             panel2.TabIndex = 1;
             // 
             // label8
@@ -118,7 +119,7 @@
             label8.AutoSize = true;
             label8.Font = new Font("Calibri", 12F);
             label8.ForeColor = Color.Black;
-            label8.Location = new Point(22, 354);
+            label8.Location = new Point(29, 358);
             label8.Name = "label8";
             label8.Size = new Size(65, 19);
             label8.TabIndex = 12;
@@ -129,7 +130,7 @@
             label7.AutoSize = true;
             label7.Font = new Font("Calibri", 12F);
             label7.ForeColor = Color.Black;
-            label7.Location = new Point(22, 293);
+            label7.Location = new Point(29, 297);
             label7.Name = "label7";
             label7.Size = new Size(41, 19);
             label7.TabIndex = 11;
@@ -140,7 +141,7 @@
             label6.AutoSize = true;
             label6.Font = new Font("Calibri", 12F);
             label6.ForeColor = Color.Black;
-            label6.Location = new Point(22, 230);
+            label6.Location = new Point(29, 234);
             label6.Name = "label6";
             label6.Size = new Size(107, 19);
             label6.TabIndex = 10;
@@ -151,7 +152,7 @@
             label5.AutoSize = true;
             label5.Font = new Font("Calibri", 12F);
             label5.ForeColor = Color.Black;
-            label5.Location = new Point(22, 166);
+            label5.Location = new Point(29, 170);
             label5.Name = "label5";
             label5.Size = new Size(110, 19);
             label5.TabIndex = 9;
@@ -163,7 +164,7 @@
             label4.AutoSize = true;
             label4.Font = new Font("Calibri", 12F);
             label4.ForeColor = Color.Black;
-            label4.Location = new Point(22, 105);
+            label4.Location = new Point(29, 109);
             label4.Name = "label4";
             label4.Size = new Size(88, 19);
             label4.TabIndex = 8;
@@ -174,7 +175,7 @@
             label3.AutoSize = true;
             label3.Font = new Font("Calibri", 12F);
             label3.ForeColor = Color.Black;
-            label3.Location = new Point(22, 42);
+            label3.Location = new Point(29, 46);
             label3.Name = "label3";
             label3.Size = new Size(83, 19);
             label3.TabIndex = 3;
@@ -187,7 +188,7 @@
             button4.FlatStyle = FlatStyle.Popup;
             button4.Font = new Font("Calibri", 12F);
             button4.ForeColor = Color.White;
-            button4.Location = new Point(22, 418);
+            button4.Location = new Point(34, 418);
             button4.Name = "button4";
             button4.Size = new Size(94, 34);
             button4.TabIndex = 7;
@@ -201,7 +202,7 @@
             button3.FlatStyle = FlatStyle.Popup;
             button3.Font = new Font("Calibri", 12F);
             button3.ForeColor = Color.White;
-            button3.Location = new Point(147, 418);
+            button3.Location = new Point(156, 418);
             button3.Name = "button3";
             button3.Size = new Size(94, 34);
             button3.TabIndex = 6;
@@ -215,60 +216,60 @@
             button2.FlatStyle = FlatStyle.Popup;
             button2.Font = new Font("Calibri", 12F);
             button2.ForeColor = Color.White;
-            button2.Location = new Point(270, 418);
+            button2.Location = new Point(274, 418);
             button2.Name = "button2";
             button2.Size = new Size(94, 34);
             button2.TabIndex = 3;
             button2.Text = "Cancel";
             button2.UseVisualStyleBackColor = false;
             // 
-            // textBox5
+            // txtBQuantity
             // 
-            textBox5.Location = new Point(180, 351);
-            textBox5.Multiline = true;
-            textBox5.Name = "textBox5";
-            textBox5.Size = new Size(184, 31);
-            textBox5.TabIndex = 5;
+            txtBQuantity.Location = new Point(148, 351);
+            txtBQuantity.Multiline = true;
+            txtBQuantity.Name = "txtBQuantity";
+            txtBQuantity.Size = new Size(219, 31);
+            txtBQuantity.TabIndex = 5;
             // 
-            // textBox6
+            // txtBPrice
             // 
-            textBox6.Location = new Point(180, 290);
-            textBox6.Multiline = true;
-            textBox6.Name = "textBox6";
-            textBox6.Size = new Size(184, 31);
-            textBox6.TabIndex = 4;
+            txtBPrice.Location = new Point(148, 290);
+            txtBPrice.Multiline = true;
+            txtBPrice.Name = "txtBPrice";
+            txtBPrice.Size = new Size(219, 31);
+            txtBPrice.TabIndex = 4;
             // 
-            // textBox7
+            // txtBDate
             // 
-            textBox7.Location = new Point(180, 227);
-            textBox7.Multiline = true;
-            textBox7.Name = "textBox7";
-            textBox7.Size = new Size(184, 31);
-            textBox7.TabIndex = 3;
+            txtBDate.Location = new Point(148, 227);
+            txtBDate.Multiline = true;
+            txtBDate.Name = "txtBDate";
+            txtBDate.Size = new Size(219, 31);
+            txtBDate.TabIndex = 3;
             // 
-            // textBox4
+            // txtBPublisher
             // 
-            textBox4.Location = new Point(180, 163);
-            textBox4.Multiline = true;
-            textBox4.Name = "textBox4";
-            textBox4.Size = new Size(184, 31);
-            textBox4.TabIndex = 2;
+            txtBPublisher.Location = new Point(148, 163);
+            txtBPublisher.Multiline = true;
+            txtBPublisher.Name = "txtBPublisher";
+            txtBPublisher.Size = new Size(219, 31);
+            txtBPublisher.TabIndex = 2;
             // 
-            // textBox3
+            // txtBAuthor
             // 
-            textBox3.Location = new Point(180, 102);
-            textBox3.Multiline = true;
-            textBox3.Name = "textBox3";
-            textBox3.Size = new Size(184, 31);
-            textBox3.TabIndex = 1;
+            txtBAuthor.Location = new Point(148, 102);
+            txtBAuthor.Multiline = true;
+            txtBAuthor.Name = "txtBAuthor";
+            txtBAuthor.Size = new Size(219, 31);
+            txtBAuthor.TabIndex = 1;
             // 
-            // textBox2
+            // txtBName
             // 
-            textBox2.Location = new Point(180, 39);
-            textBox2.Multiline = true;
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(184, 31);
-            textBox2.TabIndex = 0;
+            txtBName.Location = new Point(148, 39);
+            txtBName.Multiline = true;
+            txtBName.Name = "txtBName";
+            txtBName.Size = new Size(219, 31);
+            txtBName.TabIndex = 0;
             // 
             // dataGridView1
             // 
@@ -276,26 +277,39 @@
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dataGridView1.Location = new Point(0, 124);
             dataGridView1.Name = "dataGridView1";
-            dataGridView1.Size = new Size(505, 408);
+            dataGridView1.Size = new Size(763, 410);
             dataGridView1.TabIndex = 2;
+            dataGridView1.CellClick += dataGridView1_CellClick;
             // 
             // panel3
             // 
             panel3.BackColor = Color.LightSalmon;
+            panel3.Controls.Add(label9);
             panel3.Controls.Add(textBox1);
             panel3.Controls.Add(btnRefresh);
             panel3.Controls.Add(label2);
             panel3.Location = new Point(0, 59);
             panel3.Name = "panel3";
-            panel3.Size = new Size(505, 66);
+            panel3.Size = new Size(763, 66);
             panel3.TabIndex = 3;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Calibri", 12F);
+            label9.ForeColor = Color.White;
+            label9.Location = new Point(102, 26);
+            label9.Name = "label9";
+            label9.Size = new Size(83, 19);
+            label9.TabIndex = 13;
+            label9.Text = "Book Name";
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(150, 18);
+            textBox1.Location = new Point(238, 18);
             textBox1.Multiline = true;
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(188, 35);
+            textBox1.Size = new Size(262, 35);
             textBox1.TabIndex = 2;
             // 
             // btnRefresh
@@ -305,13 +319,12 @@
             btnRefresh.FlatStyle = FlatStyle.Popup;
             btnRefresh.Font = new Font("Calibri", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
             btnRefresh.ForeColor = Color.White;
-            btnRefresh.Location = new Point(354, 18);
+            btnRefresh.Location = new Point(559, 18);
             btnRefresh.Name = "btnRefresh";
             btnRefresh.Size = new Size(115, 35);
             btnRefresh.TabIndex = 1;
             btnRefresh.Text = "Refresh";
             btnRefresh.UseVisualStyleBackColor = false;
-            btnRefresh.Click += btnRefresh_Click;
             // 
             // label2
             // 
@@ -320,15 +333,14 @@
             label2.ForeColor = Color.White;
             label2.Location = new Point(45, 26);
             label2.Name = "label2";
-            label2.Size = new Size(83, 19);
+            label2.Size = new Size(0, 19);
             label2.TabIndex = 0;
-            label2.Text = "Book Name";
             // 
             // View_book_form
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(878, 533);
+            ClientSize = new Size(1146, 533);
             Controls.Add(panel3);
             Controls.Add(dataGridView1);
             Controls.Add(panel2);
@@ -336,6 +348,7 @@
             Name = "View_book_form";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "View_book_form";
+            Load += View_book_form_Load;
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
@@ -355,7 +368,7 @@
         private Panel panel2;
         private DataGridView dataGridView1;
         private Panel panel3;
-        private TextBox textBox2;
+        private TextBox txtBName;
         private TextBox textBox1;
         private Button btnRefresh;
         private Label label2;
@@ -366,12 +379,13 @@
         private Button button4;
         private Button button3;
         private Button button2;
-        private TextBox textBox5;
-        private TextBox textBox6;
-        private TextBox textBox7;
-        private TextBox textBox4;
-        private TextBox textBox3;
+        private TextBox txtBQuantity;
+        private TextBox txtBPrice;
+        private TextBox txtBDate;
+        private TextBox txtBPublisher;
+        private TextBox txtBAuthor;
         private Label label8;
         private Label label7;
+        private Label label9;
     }
 }
--- View book form.cs
+++ View book form.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -25,6 +26,51 @@ namespace Library_Management_System
         private void btnRefresh_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void View_book_form_Load(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = "Data Source=DESKTOP-NCOL2RG\\SQLEXPRESS;database =My2025;Integrated Security=True";
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+
+            cmd.CommandText = "select * from NewBook";
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            sda.Fill(ds);
+
+            dataGridView1.DataSource = ds.Tables[0];
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+        int bid;
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value != null)
+            {
+                bid = int.Parse(dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString());
+            }
+
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = "Data Source=DESKTOP-NCOL2RG\\SQLEXPRESS;database =My2025;Integrated Security=True";
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+
+            cmd.CommandText = "select * from NewBook where bid ="+bid+"";
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            sda.Fill(ds);
+
+            txtBName.Text = ds.Tables[0].Rows[0][1].ToString();
+            txtBAuthor.Text = ds.Tables[0].Rows[0][2].ToString();
+            txtBPublisher.Text = ds.Tables[0].Rows[0][3].ToString();
+            txtBDate.Text = ds.Tables[0].Rows[0][4].ToString();
+            txtBPrice.Text = ds.Tables[0].Rows[0][5].ToString();
+            txtBQuantity.Text = ds.Tables[0].Rows[0][6].ToString();
         }
     }
 }
